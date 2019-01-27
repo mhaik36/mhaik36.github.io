b10b84b24e2eb38b5ad3bdcd6182a3c364cfd10f
mhaik36 <46237956+mhaik36@users.noreply.github.com>
2019-01-27 11:46:19 +0700

    Add files via upload

---
+++ precache-manifest.8af3712fb622d1e302c43f1bcb911dbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d23c34cff7f96740b25a",
+    "url": "/static/css/main.c5ef807c.chunk.css"
+  },
+  {
+    "revision": "d23c34cff7f96740b25a",
+    "url": "/static/js/main.d23c34cf.chunk.js"
+  },
+  {
+    "revision": "effe1b9996f7dcea4ddd",
+    "url": "/static/js/1.effe1b99.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f18c37ea234f3fbb3338f979b772fe09",
+    "url": "/index.html"
+  }
+];
